fc8c5e67b1762386a2dc417e34c4c2f281462ed8
Ernestas <ernyszz7@gmail.com>
2022-02-12 18:57:14 +0200

    Add particles

---
+++ out/_next/static/RoGLe0eXlEMB79wCB65Dm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/629-2381eec66c3eacc5465e.js","static/chunks/pages/index-c09ad70da364c3e044dd.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
